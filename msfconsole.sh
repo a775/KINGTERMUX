60a575f03ef551b4fd841143447dcaf7ce822b3d
a775 <50821355+a775@users.noreply.github.com>
2019-06-13 08:46:16 +0300

    Create msfconsole.sh

---
+++ msfconsole.sh
@@ -0,0 +1,17 @@
+
+sleep 2
+figlet KING TERMUX
+sleep 2
+apt-get update 
+sleep 2
+apt-get upgrade
+sleep 2
+pkg install wget
+sleep 2
+termux-setup-storage
+sleep 2
+pkg install unstable-repo
+sleep 2
+pkg install metasploit
+sleep 1
+msfconsole
